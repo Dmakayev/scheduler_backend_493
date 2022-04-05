4d1380f129c167b7b9d4b6e6eff0ce3de58a7088
Dmakayev <dimitri.makayev@gmail.com>
2022-04-04 23:45:10 -0500

    Updating backend for Contacts CRUD tool - Updated serializers.py and views.py for FacultyNames

--- FacultyNames/serializers.py
+++ FacultyNames/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from FacultyNames.models import Faculty
 
+
 class FacultySerializer(serializers.ModelSerializer):
     class Meta:
         model = Faculty
-        fields = ('lastName', 'firstName', 'email', 'phone', 'department', 'title')
+        fields = ('id', 'lastName', 'firstName', 'email', 'phone', 'department', 'title')
