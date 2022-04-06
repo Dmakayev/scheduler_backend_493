a3563c1e6fd18a9a21c17f58e8f048b9d2c014f4
tjballworm <44422907+tjballworm@users.noreply.github.com>
2022-04-06 18:10:30 -0500

    Parser fix

--- parser/ParsePDF.py
+++ parser/ParsePDF.py
@@ -17,40 +17,24 @@ def parsePDF (deptInfo, outFile):
             del (lines[index])
             if index >= len(lines):
                break 
-      #   if index < len(lines):
-      #      print (format (index, "3d"), lines[index])
          index += 1
    
    
       classPattern = format (entry['prefix'], "4s") + " \d\d\d"  #'CSCI \d\d\d'
       classPattern2 = "^" + format (entry['prefix'], "4s") + " \d\d\d"  #'^CSCI \d\d\d'
-      #csClassMatcher = re.search (classPattern, text)
-      #while csClassMatcher != None:
-         #print ("class starts at ", csClassMatcher.start())   
-         #print ("class ends   at ", csClassMatcher.end())   
-         #print ("class name      ", text[csClassMatcher.span()[0] : csClassMatcher.span()[1]])
-         #print ("class name      ", csClassMatcher.group())
-         #csClassMatcher = re.search (csClassPattern, text)
-   
-      #for c in re.finditer (csClassPattern, text):
-         #print ("class starts at ", c.start())   
-         #print ("class ends   at ", c.end())   
-      #   print ("class name      ", c.group(0))
-   
-      #
+      
       index = 0
       while index < len(lines):
          if re.search(classPattern2, lines[index]) != None:
             #print (lines[index])
-            #classNumber = lines[index][0:8].strip() #excludes lab sections as a separate class
+            #classNumber = lines[index][0:8].strip() `
             classNumber = lines[index][0:9].strip() #included lab sections as a separate class
             className = lines[index][12:43].strip()
             credits = lines[index][44]
-            if classes.get (classNumber) == None:
-               classes[classNumber] = {}
-               classes[classNumber]['classNumber'] = classNumber
-               classes[classNumber]['className'] = className
-               classes[classNumber]['credits'] = credits
+            classes[classNumber] = {}
+            classes[classNumber]['classNumber'] = classNumber
+            classes[classNumber]['className'] = className
+            classes[classNumber]['credits'] = credits
             index = index + 1
             classes[classNumber]['sections'] = []
             while re.search ('^\s{10}', lines[index]) != None:
@@ -67,8 +51,8 @@ def parsePDF (deptInfo, outFile):
                capacity = lines[index][79:82].strip()
                enrollment = lines[index][86:89].strip()
                waitSize = lines[index][93:96].strip()
-               classes[classNumber]['sections'].append({'sectionNumber:': section, 'capacity': capacity, 'room:':room, 'waitSize': waitSize, 'enrollment':enrollment, 'days:':days,
-                                                       'number:': number, 'time': time, 'instructor': instructor})
+               classes[classNumber]['sections'].append({'sectionNumber': section, 'capacity': capacity, 'room':room, 'waitSize': waitSize, 'enrollment':enrollment, 'days':days,
+                                                       'number': number, 'time': time, 'instructor': instructor})
                index = index + 3
                while index < len(lines) and lines[index][0:5] == "     ":
                   index = index + 1
@@ -76,9 +60,13 @@ def parsePDF (deptInfo, outFile):
                   
          else:   
             index = index + 1   
-   outFile =   open (outFile, "w")
-   json.dump(classes, outFile)
+
+   f = list(classes.keys())
+   q = list(map(lambda field: classes[field], f))
+   outFile = open(outFile, "w")
+   json.dump(q, outFile)
    outFile.close()
+      
    
 def main ():
    deptInfo = [
